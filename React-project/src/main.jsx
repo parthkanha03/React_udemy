ce28b2a2cd2c1e869c86dcd9fe2ebff8408fa2e8
paragraw <paragraw@visa.com>
2026-05-25 15:34:25 +0530

    new commit

---
+++ React-project/src/main.jsx
@@ -0,0 +1,14 @@
+import { createRoot } from 'react-dom/client'
+import React, { useState } from 'react'
+
+
+const rootEL = document.getElementById('root1')
+
+const h1ele = document.createElement('h1')
+h1ele.textContent = "Hello Parth";
+rootEL.appendChild(h1ele)
+
+
+
+
+
